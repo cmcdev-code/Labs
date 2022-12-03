e7dafa8c84afcbdda710350c4b46dbd1f260ead1
cmcdev-code <collin20482@gmail.com>
2022-12-02 19:38:48 -0500

    .

--- CSCN Collin McDevitt Lab 8/main.cpp
+++ CSCN Collin McDevitt Lab 8/main.cpp
@@ -8,19 +8,19 @@ void loopThroughFile(string arrayOfColors[],string arrayOfGlossLvl[],string arra
 bool validDatainFile(string dataFromFiile[],int length);
 string askForPaintColor();
 double rngNum(double lower,double upper);
-bool validGlossLvl(string glossLvl[]);
+bool validGlossLvl(string glossLvl[],int length);
 
 paint* typeCreation(string color,double gloss_level,double inventory);
+paint* typeCreation(int choice);
 
+double userInput(string txtOut,double lower,double upper);
+int userInput(string txtOut,int lower,int upper);
+ 
 int main(void)
 {
     paint *  allPaintStuff[7];
     string arrayOfColors[7];string arrayOfGlossLvl[7];string arrayOfInventory[7];
-    for(int i=0;i<7;i++){
-        arrayOfColors[i]="RED";
-        arrayOfGlossLvl[i]="1.2";
-        arrayOfInventory[i]="1.2";
-    }
+    
   
 
 
@@ -46,7 +46,7 @@ void readInFromFile(string arrayOfColors[],string arrayOfGlossLvl[],string array
             loopThroughFile(arrayOfColors,arrayOfGlossLvl,arrayOfInventory,in);
 
             if(validDatainFile(arrayOfGlossLvl,7)&& validDatainFile(arrayOfInventory,7)){
-                if(validGlossLvl(arrayOfGlossLvl)){
+                if(validGlossLvl(arrayOfGlossLvl,7)){
                     break;
                 }
             }
@@ -115,13 +115,79 @@ double rngNum(double lower,double upper)
     std::uniform_real_distribution<double> randomNum(lower,upper);
     return randomNum(num);
 }
-bool validGlossLvl(string glossLvl[])
+bool validGlossLvl(string glossLvl[],int length)
 {  bool valid =true;
-    for(int i=0;i<7;i++){
+    for(int i=0;i<length;i++){
         double num=stod(glossLvl[i]);
         if(num<0.1||(num>5 and num<10.5)||(num>25 and num<25.5)||(num>35 and num<35.5)||(num>70 and num<85)||(num>95)){
             valid= false;
         }
     }
     return valid;
+}
+paint* typeCreation(int choice)
+{
+    
+    string color=askForPaintColor();
+    double inventoryLeft=userInput("How much inventory is left?",0,10000);
+    if(choice==1){
+    return new flat(color,rngNum(0.1,5.0),inventoryLeft);
+    }
+    elif(choice==2){
+    return new Eggshell(color,rngNum(10.5,25.0),inventoryLeft);
+    }
+    elif(choice==3){
+    return new satin(color,rngNum(25.5,35.0),inventoryLeft);
+    }
+    elif(choice==4){
+    return new semiGloss(color,rngNum(35.5,70.0),inventoryLeft);
+    }
+    elif(choice==5){
+    return new highGloss(color,rngNum(85.0,95.0),inventoryLeft);
+    }
+    else{
+        return nullptr;
+    }   
+}
+double userInput(string txtOut,double lower,double upper){
+ double userIn;
+    while (true)
+    {
+        cout << txtOut + "\n";
+        cin >> userIn;
+        if (userIn >= lower && userIn <= upper && !cin.fail())
+        {
+            break;
+        }
+        else
+        {
+            cout << "Invalid input please enter a number between " << lower << " and " << upper << "\n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+    }
+
+    return userIn;
+}
+int userInput(string txtOut,int lower,int upper){
+ int userIn;
+    while (true)
+    {
+        cout << txtOut + "\n";
+        cin >> userIn;
+        if (userIn >= lower && userIn <= upper && !cin.fail())
+        {
+            break;
+        }
+        else
+        {
+            cout << "Invalid input please enter a number between " << lower << " and " << upper << "\n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+    }
+
+    return userIn;
 }
